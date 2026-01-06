f48fc2561e1a3b9511f77fe7b15bed90c0d0913b
morris <morrisx28@gmail.com>
2026-01-06 18:05:02 +0800

    add little white

--- simulate_python/config.py
+++ simulate_python/config.py
@@ -1,4 +1,4 @@
-ROBOT = "pineapple" # Robot name, "go2", "b2", "b2w", "h1", "go2w", "g1" 
+ROBOT = "little_white" # Robot name, "go2", "b2", "b2w", "h1", "go2w", "g1" 
 ROBOT_SCENE = "../unitree_robots/" + ROBOT + "/scene.xml" # Robot scene
 DOMAIN_ID = 1 # Domain id
 INTERFACE = "lo" # Interface 
